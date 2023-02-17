1f58fcf7ac6d224e304b68f1f9b3a54db8e3fbf1
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-17 11:07:05 +0600

    email field value getting method added

--- js/login.js
+++ js/login.js
@@ -2,7 +2,10 @@
 // Step-1: add click event handler on submit button
 document.getElementById("btn-submit").addEventListener('click', function(){
   
-// Step-2: Get the email input field value
-  
+ // Step-2: Get the email input field value
+  const emailFelid = document.getElementById("user-email");
+  const email = emailFelid.value;
+  console.log(email);
+
 })
 
