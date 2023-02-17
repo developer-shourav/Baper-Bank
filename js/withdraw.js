da94480fd339eea04e91cb20b3177d2aced7becc
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-17 22:51:26 +0600

    withdraw method added

---
+++ js/withdraw.js
@@ -0,0 +1,13 @@
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const withdrawField = document.getElementById("withdraw-field");
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    
+    const withdrawTotalElement = document.getElementById("withdraw-total");
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+})
