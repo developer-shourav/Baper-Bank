eed0d405ba3dd4dcaa6050ce49c144d0bf35fc65
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-17 13:18:30 +0600

    deposite file valu updated

--- js/deposit.js
+++ js/deposit.js
@@ -2,5 +2,10 @@
 document.getElementById("btn-deposit").addEventListener('click', function(){
   const depositField = document.getElementById('deposit-field');
   const depositAmount = depositField.value;
-  console.log(depositAmount);
+  // step:3
+  const primaryDeposit = document.getElementById('deposit-total');
+  const primaryDepositValue = primaryDeposit.innerText;
+  primaryDeposit.innerText = depositAmount;
 });
+
+
