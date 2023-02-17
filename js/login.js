b06451d8ff552c630cb54a82a2066de127919e87
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-17 11:02:09 +0600

    submit btn click event handler added

---
+++ js/login.js
@@ -0,0 +1,8 @@
+
+// Step-1: add click event handler on submit button
+document.getElementById("btn-submit").addEventListener('click', function(){
+  
+// Step-2: Get the email input field value
+  
+})
+
